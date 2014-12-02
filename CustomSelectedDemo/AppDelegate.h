c6052adec2ca2f9a510b1a7dd4ae5a7dee23497e
sunbohong <sunbohong@gmail.com>
2014-12-02 15:15:39 +0800

    Initial Commit

---
+++ CustomSelectedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomSelectedDemo
+//
+//  Created by 孙博弘 on 14/12/2.
+//  Copyright (c) 2014年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomSelectedDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomSelectedDemo
+//
+//  Created by 孙博弘 on 14/12/2.
+//  Copyright (c) 2014年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
